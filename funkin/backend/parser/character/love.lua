5a3453902d1e11c79f87683b9086eb869c9e2cc8
VicKaoy <vk15@tuta.io>
2024-11-09 19:17:21 -0300

    remove this

--- funkin/backend/parser/character/love.lua
+++ funkin/backend/parser/character/love.lua
@@ -1,21 +1,5 @@
 local love = {}
-{
-	"animations": [
-		["singLEFT", "left", [], 24, false, [2, -2]],
-		["singDOWN", "down", [], 24, false, [0, 1]],
-		["singUP", "up", [], 24, false, [0, 0]],
-		["singRIGHT", "right", [], 24, false, [-4, 0]],
-		["idle", "idle", [], 24, false, [0, 0]]
-	],
-	"position": [-260, -60],
-	"camera_points": [-40, 100],
-	"flip_x": false,
-	"icon": "4j",
-	"sprite": "characters/4j",
-	"antialiasing": true,
-	"sing_duration": 4,
-	"scale": 1
-}
+
 function love.parse(data)
 	local char = table.clone(love.base)
 
